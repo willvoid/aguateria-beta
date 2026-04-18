8979b8e698931c54b096422f70493123a6cdcdbf
willvoid <willmvb1787@gmail.com>
2026-04-18 19:55:49 -0300

    Second commit

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "664116223"
+    serviceWorkerVersion: "1454844743"
   }
 });
